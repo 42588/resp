74a73b56684281ef427b2f7e903a44994d86557e
生拌君 <y999_qq@163.com>
2023-02-05 00:55:08 +0800

    第一次项目 yaml文件

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.bean.Person;
 
 @SpringBootTest
 class DemoApplicationTests {
-
+	@Autowired
+	Person person;
 	@Test
 	void contextLoads() {
+		System.out.println(person);
 	}
 
 }
