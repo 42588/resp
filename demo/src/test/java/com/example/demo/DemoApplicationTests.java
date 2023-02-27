d01f8297d03d015d9ebc4e9510141ab63bc3c605
生拌君 <y999_qq@163.com>
2023-02-27 22:58:37 +0800

    比较

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,28 +1,13 @@
 package com.example.demo;
 
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-
-import com.example.demo.bean.Person;
-import com.example.demo.dao.ServiceDao;
 
 @SpringBootTest
 class DemoApplicationTests {
-	@Autowired
-	Person person;
-	@Autowired
-	ApplicationContext ioc;
+
 	@Test
 	void contextLoads() {
-		System.out.println(person);
 	}
-	@Test
-	void testConfig(){
-		System.out.println("--------------");
-		ServiceDao bean = ioc.getBean(ServiceDao.class);
-		bean.sayHello();
-	}
+
 }
--- ruiji/src/main/java/cn/hbmz/ruiji/controller/EmployeeController.java
+++ ruiji/src/main/java/cn/hbmz/ruiji/controller/EmployeeController.java
@@ -1,14 +1,40 @@
 package cn.hbmz.ruiji.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.hbmz.ruiji.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import cn.hbmz.ruiji.common.R;
+import cn.hbmz.ruiji.entity.Employee;
+
+@Slf4j
 @RequestMapping("/employee")
 @RestController
 public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
-    
+
+    /**
+     * 员工登录
+     * 
+     * @param employee
+     * @param request
+     * @return
+     */
+    @PostMapping("/login")
+    public R<Employee> Login(@RequestBody Employee employee, HttpServletRequest request) {
+        // 将页面提交的密码进行md5加密
+        // 根据用户提交的username查询数据库
+        // 如果查询失败就返回失败的的结果
+        // 比对密码，如果不一致就返回失败的结果
+        // 查看员工的状态，是否可用
+        // 登录成功，将id放入到session中
+        return null;
+    }
 }
