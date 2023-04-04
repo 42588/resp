939aeb96165eefee0db3bf5014095c8b6d9d58e7
生拌君 <y999_qq@163.com>
2023-04-05 00:02:12 +0800

    百度地图学习0405

---
+++ game/itcast_baidumap/src/main/java/com/example/itcast_baidumap/ItcastBaidumapApplication.java
@@ -0,0 +1,13 @@
+package com.example.itcast_baidumap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcastBaidumapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItcastBaidumapApplication.class, args);
+	}
+
+}
---
+++ game/itcast_baidumap/src/test/java/com/example/itcast_baidumap/ItcastBaidumapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itcast_baidumap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcastBaidumapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
